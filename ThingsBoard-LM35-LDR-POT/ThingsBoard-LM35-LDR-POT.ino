0be656217b919fc9b7db290d658cb998979f4e3a
Ahmad Saparhadi <saparhadi@gmail.com>
2019-10-29 13:06:41 +0700

    Create

---
+++ ThingsBoard-LM35-LDR-POT/ThingsBoard-LM35-LDR-POT.ino
@@ -0,0 +1,61 @@
+#include "ThingsBoard.h"
+const int potentioPin = 32;
+const int ldrPin = 33;
+const int lm35Pin = 34;
+int potentioValue;
+int ldrValue;
+float lm35Value;
+float Temp, IntTemp;
+String info;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+uint8_t temprature_sens_read();
+#ifdef __cplusplus
+}
+#endif
+uint8_t temprature_sens_read();
+
+void setup()
+{
+  thingsboard_Init();
+}
+
+void loop()
+{
+  String data;
+  const size_t bufferSize = JSON_OBJECT_SIZE(3);
+  DynamicJsonBuffer jsonBuffer(bufferSize);
+  JsonObject& root = jsonBuffer.createObject();
+  
+  potentioValue = analogRead(potentioPin);
+  ldrValue = analogRead(ldrPin);
+  
+  lm35Value = analogRead (lm35Pin);
+  Temp = (lm35Value/4095)*500;
+  IntTemp = ((temprature_sens_read() - 32) / 1.8);
+  //Temp = map(lm35Value, 0, 4095, 2, 150);
+
+  if (Temp <14.9){
+    info = "Dingin";
+  } else if (Temp >=15 && Temp <21){
+      info = "Agak dingin";
+  } else if (Temp >=21 && Temp <26){
+      info = "Normal";
+  } else if (Temp >=26 && Temp <30){
+      info = "Agak Hangat";
+  } else {
+    info = "Panas";
+    }
+  
+  root["pot "] = String(potentioValue);
+  root["lux "] = String(ldrValue);
+  root["C "] = String(Temp);
+  root["C* "] = String(IntTemp);
+  root["info"] = String(info);
+  
+  root.printTo(data);
+  thingsboard_Publish(data);
+  client.loop();
+}
